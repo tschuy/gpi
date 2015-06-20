1ea3a8283f1d92c4ef60c2b9aacde22e3380f3ea
Evan Tschuy <evantschuy@gmail.com>
2015-06-20 01:43:29 -0700

    Install/uninstall manifest file

--- installer.py
+++ installer.py
@@ -4,34 +4,52 @@ import json
 gimp_plugins_dir = os.environ.get(
     'GIMP_PLUGIN_DIR', os.path.expanduser("~/.gimp-2.8/plug-ins/"))
 
+gpi_config_file = gimp_plugins_dir + '.gpi.json'
 
-def plugin_config_file(plugin_name):
-    return gimp_plugins_dir + '.' + plugin_name
+
+def is_non_zero_file(path):
+    return True if os.path.isfile(path) and os.path.getsize(path) > 0 else False
 
 
 def install(tar, manifest):
-    with open(plugin_config_file(manifest['identifier']), 'w') as f:
-        files = [t for t in tar if t.name != 'gpi.json']
-        plugin_info = {
-            'version': manifest['version'],
-            'name': manifest['name'],
-            'files': [file.name for file in files]
-        }
-        f.write(json.dumps(plugin_info))
+    if not is_non_zero_file(gpi_config_file):
+        index = {}
+    else:
+        with open(gpi_config_file, 'r') as f:
+            index = json.load(f)
+
+    files = [t for t in tar if t.name != 'gpi.json']
+    plugin_info = {
+        'version': manifest['version'],
+        'name': manifest['name'],
+        'files': [file.name for file in files]
+    }
+    index[manifest['identifier']] = plugin_info
+
+    with open(gpi_config_file, 'w') as f:
+        f.write(json.dumps(index))
 
     tar.extractall(gimp_plugins_dir, members=files)
 
 
-def uninstall(config):
-    with open(config, 'r') as f:
-        data = json.load(f)
+def uninstall(plugin_name):
+    with open(gpi_config_file, 'r') as f:
+        index = json.load(f)
+        if plugin_name not in index:
+            f.write(json.dumps(index))
+            return False
 
-        directories = []
-        for file in data['files']:
-            if os.path.isdir(gimp_plugins_dir + file):
-                directories.append(gimp_plugins_dir + file)
-            else:
-                os.remove(gimp_plugins_dir + file)
-
+    directories = []
+    for file in index[plugin_name]['files']:
         # TODO remove empty directories
-        os.remove(config)
+        if os.path.isdir(gimp_plugins_dir + file):
+            directories.append(gimp_plugins_dir + file)
+        else:
+            os.remove(gimp_plugins_dir + file)
+
+    del index[plugin_name]
+    print index
+    with open(gpi_config_file, 'w') as f:
+        f.write(json.dumps(index))
+
+    return True
