1f5270da10c48c2373086d6d281246b4ce9c4442
Evan Tschuy <evantschuy@gmail.com>
2015-06-20 01:03:20 -0700

    Add support for installing from gpi-web server

--- web.py
+++ web.py
@@ -1,7 +1,38 @@
+import io
 import os
+import urllib2
+import json
+import tarfile
 
-host = os.environ.get('GPI_HOST', "http://localhost:8000/")
+host = os.environ.get('GPI_HOST', "http://localhost:8000")
 
-def get_from_web(package_name, host):
-    response = urllib2.urlopen(host + args.command[1] + '.tar.gz')
-    print response.read()
+class PackageNotFound(Exception):
+    """
+    The exception thrown if the package 404s
+    """
+
+
+class PackageReadError(Exception):
+    def __init__(self, url):
+        super(PackageReadError, self).__init__(url)
+    """
+    The exception thrown if the package cannot be read
+    """
+
+
+def get_from_web(package_name):
+    url = "{}/api/package/{}".format(host, package_name)
+    try:
+        response = urllib2.urlopen(url)
+    except urllib2.HTTPError as error:
+        raise PackageNotFound
+
+    package_info = json.loads(response.read())
+    url = "{}{}".format(
+        host, package_info['releases'][0]['file'])
+    file = urllib2.urlopen(url).read()
+
+    try:
+        return tarfile.open(fileobj=io.BytesIO(file), mode='r')
+    except:
+        raise PackageReadError(url)
