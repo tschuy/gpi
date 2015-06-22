a7aed7e953e3981ea6b3bcb37c12d71c59ce40e1
Evan Tschuy <evantschuy@gmail.com>
2015-06-21 23:46:37 -0700

    Add installer.install tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,36 @@
+import os
+import json
+import shutil
+import tarfile
 import unittest
 
+from gpi import installer, web
 
-class GPITest(unittest.TestCase):
-    def test(self):
-        self.assertEqual(1, 1)
+class InstallerTest(unittest.TestCase):
+    def setUp(self):
+        self.current_dir = os.path.dirname(os.path.realpath(__file__))
+        self.test_dir = os.path.join(self.current_dir, 'testdir')
+        os.makedirs(self.test_dir)
+
+        installer.gpi_config_file = os.path.join(self.test_dir, '.gpi.json')
+        installer.gimp_plugins_dir = self.test_dir
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir)
+
+
+    def test_install(self):
+        t = tarfile.open(os.path.join(self.current_dir,
+            'data', 'imguruploader.tar.gz'), 'r')
+        manifest = json.load(t.extractfile('gpi.json'))
+
+        installer.install(t, manifest)
+
+        with open(os.path.join(self.test_dir, '.gpi.json'), 'r') as index:
+            expected_index = {"imguruploader": {
+                                  "files": ["upload.py"],
+                                  "version": "0.1.0",
+                                  "name": "Imgur Uploader"}}
+            self.assertEqual(json.load(index), expected_index)
+        self.assertTrue(os.path.isfile(
+            os.path.join(self.test_dir, '.gpi.json')))
