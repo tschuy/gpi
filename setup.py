a05a5cd19703de5cfb4cb2a4b25fd8560240692d
Evan Tschuy <evantschuy@gmail.com>
2015-06-21 19:41:43 -0700

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='gpi',
-    version='0.1.0',
+    version='0.1.1',
     author=u'Evan Tschuy',
     author_email='evantschuy@gmail.com',
     packages=find_packages(),
